d5ae03924f6d7f9724570c5c6830569a4aeb9877
Joe Hubbard <joehubbard46@googlemail.com>
2019-02-24 23:41:46 +0000

    Commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,9 @@
 const mix = require('laravel-mix');
 
-mix.js('assets/js/main.js', 'public/js')
+mix.js([
+    'assets/js/main.js',
+    'assets/js/embed.js'
+    ], 'public/js')
    .sass('assets/scss/main.scss', 'public/css')
    .options({processCssUrls: false });
 
