c1b23bd219b69cc2e653731aff70579d81bc1df9
Joe Hubbard <joehubbard46@googlemail.com>
2019-11-14 22:02:49 +0000

    Header, nav and intro draft

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+import $ from 'jquery'
+
+$(document).ready(function($){
+
+    $('.hamburger')
+        .hover(
+            function(element){
+                $(this).addClass('hover');
+            }, 
+            function(element){
+                $(this).removeClass('hover');
+            })
+        .click(
+            function(){
+                if( $(this).hasClass('open') ){
+                    $(this).addClass('hover');
+                }
+                else{
+                    $(this).removeClass('hover');
+                }
+
+                $(this).toggleClass('open');
+                $('.body__wrapper').toggleClass('navActive');
+                
+            }
+        );
+
+
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 const mix = require('laravel-mix');
 
 mix.js([
-    'assets/js/main.js',
-    'assets/js/embed.js'
-    ], 'public/js')
+    'node_modules/jquery/dist/jquery.min.js',
+    'assets/js/main.js'
+    ], 'public/js/main.js')
    .sass('assets/scss/main.scss', 'public/css')
    .options({processCssUrls: false });
 
